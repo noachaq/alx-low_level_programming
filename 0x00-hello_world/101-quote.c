22bbfbfb25503e4782a6814392e961b01b31901f
noachaq <nourdineach0@gmail.com>
2023-03-16 14:12:01 -0700

    alx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+
 /**
  * main - Entry point
- *
- * Return: Always 1 (Success)
- */
+ * Return: This time we return an error 1.
+*/
+
 int main(void)
 {
-fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
